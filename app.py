6852f609d22d38cfbeade2a555ba9f26d4ba55db
Joran Le Quellec <joran.lequellec@gmail.com>
2023-02-23 08:36:26 +1300

    ruined SQLalchemy

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, g, request, redirect, url_for, session, flash
-from sqlalchemy import create_engine, select, insert, update, delete
-from sqlalchemy.orm import sessionmaker
-from entities import *
-import base
-import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+import os, hashlib
+
+import models
 
 
 app = Flask(__name__, template_folder="templates")
-engine = create_engine('sqlite:///database/qa.db', echo = True)
-session = sessionmaker(bind=engine)()
-base.Base.metadata.create_all(engine)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/qa.db'
+app.secret_key = os.getenv('SECRETKEY')
+db = SQLAlchemy(app)
+
+
+SALT = os.getenv('SALT')
+UPLOAD_FOLDER = 'static/images/'
 
 
 @app.route('/')
-def home():
-    s = session.query(User).all()
-    return render_template('home.html', users = s)
+def check():
+    s = models.User.query.all()
+    return redirect(url_for('home', users = s))
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
